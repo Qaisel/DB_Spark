7e885b30f068329b6de44c85b64928eda06d5c16
Qaisel <151125693+Qaisel@users.noreply.github.com>
2025-04-16 22:35:42 +0800

    Question 1 Completed

--- student_files/q1.py
+++ student_files/q1.py
@@ -1,5 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 # you may add more import if you need to
 
 
@@ -9,3 +10,15 @@ hdfs_nn = sys.argv[1]
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
 
+inputPath = f"hdfs://{hdfs_nn}:9000/assignment2/part1/input/"
+df = spark.read.option("header",True).csv(inputPath)
+
+cleanDf = df.filter((col("Reviews").isNotNull()) & (col("Reviews").cast("int")>0) & (col("Rating").isNotNull()) & (col("Rating").cast("float")>=3.0))
+
+cleanDf.write().csv("Cleaned")
+
+outputPath = f"hdfs://{hdfs_nn}:9000/assignment2/output/question1/"
+
+cleanDf.write.option("header",True).csv(outputPath)
+
+spark.stop()
