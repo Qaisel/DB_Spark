0b36d053fefe7721aea7208370998ec9b4e01537
Qaisel <151125693+Qaisel@users.noreply.github.com>
2025-04-18 13:23:09 +0800

    q1.py

--- student_files/q1.py
+++ student_files/q1.py
@@ -1,24 +1,23 @@
 import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
-# you may add more import if you need to
-
 
 # don't change this line
 hdfs_nn = sys.argv[1]
 
-spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
+spark = SparkSession.builder.appName("Assignment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
+inputPath = f"hdfs://{hdfs_nn}:9000/assignment2/part1/input/TA_restaurants_curated_cleaned.csv"
+df = spark.read.option("header", True).csv(inputPath)
 
-inputPath = f"hdfs://{hdfs_nn}:9000/assignment2/part1/input/"
-df = spark.read.option("header",True).csv(inputPath)
-
-cleanDf = df.filter((col("Reviews").isNotNull()) & (col("Reviews").cast("int")>0) & (col("Rating").isNotNull()) & (col("Rating").cast("float")>=3.0))
-
-cleanDf.write().csv("Cleaned")
-
+cleanDf = df.filter(
+    (col("Reviews").isNotNull()) & 
+    (col("Reviews").cast("int") > 0) & 
+    (col("Rating").isNotNull()) & 
+    (col("Rating").cast("float") >= 3.0)
+)
+cleanDf.show()
 outputPath = f"hdfs://{hdfs_nn}:9000/assignment2/output/question1/"
-
-cleanDf.write.option("header",True).csv(outputPath)
+cleanDf.write.option("header", True).csv(outputPath)
 
 spark.stop()
